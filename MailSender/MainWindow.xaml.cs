f3a081dc9a6cd0c5634b0cddfbbaf7edac3cedd1
Nicolay Makukhin <nnmakukhin@gmail.com>
2020-01-31 01:09:18 +0300

    homework1

---
+++ MailSender/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<MailServerInfo> Servers => MailServerInfo.Servers;
+
+        public MailServerInfo Server { get; set; }
+        public string UserName { get; set; }
+        public SecureString Password { get; set; }
+
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+            Server = Servers.FirstOrDefault();
+        }
+
+        private void ButtonSend_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Password = PasswordBox.SecurePassword;
+
+                EmailSendServiceClass.SendMail(From, To, Subject, Body, Server, UserName, Password);
+
+                var text = $"From: {From}\nTo: {To}\nSubject: {Subject}\nBody: {Body}\n\n";
+                MessageWindow.Show("Ура!", text + "Сообщение как будто отправлено, но на самом деле нет.", Brushes.Green);
+            }
+            catch (Exception exception)
+            {
+                MessageWindow.Show("Ошибка!", exception.ToString(), Brushes.Red);
+            }
+        }
+    }
+}
---
+++ MailSender/MessageWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public string Caption { get; set; }
+        public string MessageText { get; set; }
+        public Brush MessageBrush { get; set; }
+
+        public static void Show(string caption, string messageText, Brush messageBrush)
+        {
+            new MessageWindow
+            {
+                Caption = caption,
+                MessageText = messageText,
+                MessageBrush = messageBrush
+            }.ShowDialog();
+        }
+
+        public MessageWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        private void ButtonOk_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
