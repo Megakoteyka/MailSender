fb5f651d6b2c247ead2a05cdcf7a84f0b2c08c6d
Nicolay Makukhin <nnmakukhin@gmail.com>
2020-02-11 03:20:14 +0300

    + валидация

--- MailSender.Lib/Entities/NamedEntity.cs
+++ MailSender.Lib/Entities/NamedEntity.cs
@@ -1,9 +1,34 @@
-﻿namespace MailSender.Lib.Entities
+﻿using System.ComponentModel;
+
+namespace MailSender.Lib.Entities
 {
-    public class NamedEntity : Entity
+    public class NamedEntity : Entity, IDataErrorInfo
     {
         public string Name { get; set; }
 
         public override string ToString() => base.ToString() + $", Name = {Name}";
+
+        public virtual string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(Name):
+
+                        if (string.IsNullOrWhiteSpace(Name))
+                            return "Имя не может быть пустым";
+                        if (Name.Length < 2)
+                            return "Имя не может быть короче двух символов";
+                        if (Name.Length > 30)
+                            return "Имя не может быть длинее 30 символов";
+                        break;
+                }
+
+                return null;
+            }
+        }
+
+        public virtual string Error => null;
     }
 }
